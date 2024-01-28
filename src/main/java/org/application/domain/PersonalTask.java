417a5ba9181ad77ad8f66bc409d3dfbdc6e2192e
Ana Clara <aclara.bvicente@gmail.com>
2024-01-28 16:54:50 -0300

    create PersonalTask class

--- src/main/java/org/application/domain/PersonalTask.java
+++ src/main/java/org/application/domain/PersonalTask.java
@@ -1,4 +1,17 @@
 package org.application.domain;
 
-public class PersonalTask {
+import org.application.domain.enums.PersonalTaskCategory;
+import org.application.domain.enums.TaskPriority;
+import org.application.domain.enums.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class PersonalTask extends BaseTask {
+    private PersonalTaskCategory category;
+
+    public PersonalTask(String id, String taskTitle, String taskDescription, LocalDateTime createdAt, LocalDateTime updatedAt, TaskStatus taskStatus, Date dueDate, TaskPriority taskPriority, PersonalTaskCategory category) {
+        super(id, taskTitle, taskDescription, createdAt, updatedAt, taskStatus, dueDate, taskPriority);
+        this.category = category;
+    }
 }
