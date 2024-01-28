86ce3aded19ba77b0ae758b0a626b869f9b1cac3
Ana Clara <aclara.bvicente@gmail.com>
2024-01-28 18:19:01 -0300

    create Work Task class

--- src/main/java/org/application/domain/WorkTask.java
+++ src/main/java/org/application/domain/WorkTask.java
@@ -1,4 +1,34 @@
 package org.application.domain;
 
-public class WorkTask {
+import org.application.domain.enums.TaskPriority;
+import org.application.domain.enums.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class WorkTask extends BaseTask {
+    private String project;
+    private String client;
+
+    public WorkTask(String id, String taskTitle, String taskDescription, LocalDateTime createdAt, LocalDateTime updatedAt, TaskStatus taskStatus, Date dueDate, TaskPriority taskPriority, String project, String client) {
+        super(id, taskTitle, taskDescription, createdAt, updatedAt, taskStatus, dueDate, taskPriority);
+        this.project = project;
+        this.client = client;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
 }
