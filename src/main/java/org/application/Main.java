614a4873a2ad820f81a5c1f083ed688edb6e43e6
Ana Clara <aclara.bvicente@gmail.com>
2024-01-31 14:58:02 -0300

    sets main

--- src/main/java/org/application/Main.java
+++ src/main/java/org/application/Main.java
@@ -1,7 +1,14 @@
 package org.application;
 
+import org.application.controller.TaskController;
+import org.application.domain.BaseTask;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TaskController controller = new TaskController();
+
+
+
+        controller.mainMenu();
     }
 }
