e9f58933ba799a04da9403c3abb8fb4fe9bdfde1
Ana Clara <aclara.bvicente@gmail.com>
2024-01-31 00:28:46 -0300

    configure service

--- src/main/java/org/application/service/TaskService.java
+++ src/main/java/org/application/service/TaskService.java
@@ -1,4 +1,40 @@
 package org.application.service;
 
-public class TaskService {
+import org.application.domain.BaseTask;
+import org.application.repository.TaskRepository;
+
+import java.util.List;
+
+public class TaskService<T extends BaseTask, U> {
+
+    private final TaskRepository<T, U> taskRepository;
+
+    public TaskService(TaskRepository<T, U> taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public void createTask(T task) {
+        taskRepository.createTask(task);
+    }
+
+    public void deleteTask(T task) {
+        taskRepository.deleteTask(task);
+    }
+
+    public void updateTask(T task) {
+        taskRepository.updateTask(task);
+    }
+
+    public List<T> findAll() {
+        List<T> tasks = taskRepository.findAll();
+        for (T task : tasks) {
+            task.toString();
+        }
+        return tasks;
+    }
+
+    public T findById(String id) {
+        return (T) taskRepository.findById(id);
+    }
+
 }
